9e45deb4252f3e8c2ccb615a54bb1277ea9289c4
danielmced <danielmced@gmail.com>
2021-03-07 18:08:52 -0600

    Se ha cambiado requestMapping por un GetMapping

--- LiskovSubstitutionProblem/src/main/java/gt/edu/umg/liskov_substitution_problem/LiskovSubstitutionProblemApplication.java
+++ LiskovSubstitutionProblem/src/main/java/gt/edu/umg/liskov_substitution_problem/LiskovSubstitutionProblemApplication.java
@@ -2,7 +2,7 @@ package gt.edu.umg.liskov_substitution_problem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +23,7 @@ public class LiskovSubstitutionProblemApplication {
      * @return
      * @throws java.lang.Exception
      */
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(@RequestParam(defaultValue = "Dragon") String npc) throws Exception {
         String text = "";
         if("dragon".equalsIgnoreCase(npc)){
